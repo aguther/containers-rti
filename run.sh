78cb31224dafbe0c5bb3052087d2251d1a0d8d31
Andreas Guther <andreas@guther.net>
2016-12-27 15:48:42 +0100

    Added stop script

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# run publisher image
-docker run --detach=true --net=weave --name rti-perftest-pub rti-perftest:latest -pub
+# run combined images
+docker run --detach=true --name rti-perftest-pub rti-perftest:latest -pub -sleep 1000
+docker run --detach=true --name rti-perftest-sub rti-perftest:latest -sub
 
-# run subscriber image
-docker run --detach=true --net=weave --name rti-perftest-sub rti-perftest:latest -sub
+# run combined images with weave
+#docker run --detach=true --net=weave --name rti-perftest-pub rti-perftest:latest -pub -sleep 1000
+#docker run --detach=true --net=weave --name rti-perftest-sub rti-perftest:latest -sub
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# stop/delete containers
+docker rm -f rti-perftest-pub
+docker rm -f rti-perftest-sub
