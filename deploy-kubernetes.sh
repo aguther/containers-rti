042738a58320fa153d933e02adbdf466942898ed
Andreas Guther <andreas@guther.net>
2018-08-03 11:32:30 +0200

    Added bash script to easily start kubernetes cluster

---
+++ deploy-kubernetes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# define vm size and count
+export VM_CPUS=2
+export VM_MEMORY=2048
+export VM_INSTANCES=3
+
+# define playbook to run
+export PLAYBOOK=deploy-kubernetes.yml
+
+# destroy current vms
+vagrant destroy -f
+
+# create and start vms
+vagrant up
