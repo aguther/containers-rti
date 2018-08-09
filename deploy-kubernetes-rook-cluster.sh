1976eeb83a8db6a819b18a51dea2d5190a751fa9
Andreas Guther <andreas@guther.net>
2018-08-09 22:08:47 +0200

    Remove specific helper shell scripts and added one general one

--- deploy-kubernetes-rook-cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# deploy rook-cluster
-vagrant ssh -c "pushd /vagrant && ansible-playbook -i /tmp/vagrant-ansible/inventory/vagrant_ansible_local_inventory deploy-rook-cluster.yml"
