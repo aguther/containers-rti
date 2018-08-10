6532bdfdd9bc83b66e16491532727937d98c80ec
Andreas Guther <andreas@guther.net>
2018-08-10 11:41:38 +0200

    Improved run-playbook script to allow more parameters

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # run playbook
-vagrant ssh -c "pushd /vagrant && ansible-playbook -i /tmp/vagrant-ansible/inventory/vagrant_ansible_local_inventory $1"
+vagrant ssh -c "pushd /vagrant && ansible-playbook -i /tmp/vagrant-ansible/inventory/vagrant_ansible_local_inventory $*"
