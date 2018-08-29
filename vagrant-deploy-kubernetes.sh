3cb8e501211e118a0356ffab6ae9adb59d1b336b
Andreas Guther <andreas.guther@airbus.com>
2018-08-29 10:34:49 +0200

    Improved flexibility of deployment bootstrap script; print configuration

--- vagrant-deploy-kubernetes.sh
+++ vagrant-deploy-kubernetes.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
 # define vm size and count
-export VM_CPUS=2
-export VM_MEMORY=3072
-export VM_INSTANCES=4
+export VM_CPUS=${VM_CPUS:=2}
+export VM_MEMORY=${VM_MEMORY:=3072}
+export VM_INSTANCES=${VM_INSTANCES:=4}
 
 # define playbook to run
-export PLAYBOOK=deploy-kubernetes.yml
+export PLAYBOOK=${PLAYBOOK:=deploy-kubernetes.yml}
+
+# print values
+echo
+echo Deployment configuration:
+echo VM_CPUS=${VM_CPUS}
+echo VM_MEMORY=${VM_MEMORY}
+echo VM_INSTANCES=${VM_INSTANCES}
+if [[ -v VM_PROXY ]]; then
+    echo VM_PROXY=${VM_PROXY}
+fi
+echo
+echo PLAYBOOK=${PLAYBOOK}
+echo
 
 # destroy current vms
 vagrant destroy -f
