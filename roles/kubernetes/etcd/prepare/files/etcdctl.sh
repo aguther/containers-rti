85e32645cc3eb035d90a13c3be634d0a114bd80b
Andreas Guther <andreas@guther.net>
2019-06-22 09:58:04 +0200

    Changed deployment of K8S to use self hosted etcd (using kubeadm)

--- roles/kubernetes/etcd/prepare/files/etcdctl.sh
+++
@@ -1,5 +0,0 @@
-# set configuration file for etcdctl
-export ETCDCTL_CA_FILE='/etc/kubernetes/pki/etcd/ca.crt'
-export ETCDCTL_CERT_FILE='/etc/kubernetes/pki/etcd/server.crt'
-export ETCDCTL_KEY_FILE='/etc/kubernetes/pki/etcd/server.key'
-export ETCDCTL_ENDPOINTS='https://127.0.0.1:2379'
